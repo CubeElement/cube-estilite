0ea16886b1337184188b469885d3e23c412de0b7
CubeElement <cubeelement@protonmail.com>
2021-04-18 17:37:55 +0200

    Reworks the logic of signals and slots

--- mainframe.py
+++ mainframe.py
@@ -9,34 +9,106 @@ class MainWindow(QMainWindow, Ui_Form):
     def __init__(self, model=None):
         self.model = model
         QMainWindow.__init__(self)
-        self.setupUi(self)
+        self.setupUi(self)        
+        self.set_current_values()
+        self.actives_set = set()
         self.connectSignals()
 
-    def connectSignals(self):
-        self.ui_dia.editingFinished.connect(self.cuttingdata_calc)        
-        self.ui_speed.editingFinished.connect(self.cuttingdata_calc)
-        self.ui_rev.editingFinished.connect(self.cuttingdata_calc)
-
-    def speed_expr(self):
+    def set_current_values(self):
         self.diameter = self.ui_dia.text()
         self.rev = self.ui_rev.text()
-        speed_result = self.model.speed_formel(float(self.rev), float(self.diameter))
-        self.ui_speed.setText(str(speed_result))
+        self.speed = self.ui_speed.text()
+        self.feedrate = self.ui_feedrate.text()
+        self.feedpertooth = self.ui_feedpertooth.text()
+        self.znum = self.ui_z.text()
+
+    def connectSignals(self):
+        self.ui_dia.editingFinished.connect(self.diameter_slot)        
+        self.ui_speed.editingFinished.connect(self.speed_slot)
+        self.ui_rev.editingFinished.connect(self.rev_slot)
+        self.ui_z.editingFinished.connect(self.z_slot)
+        self.ui_feedrate.editingFinished.connect(self.F_slot)
+        self.ui_feedpertooth.editingFinished.connect(self.f_slot)
+    
+    def update_status(self, name=None, value=None):
+        if value!="" and name not in self.actives_set:
+            self.actives_set.add(str(name))
+        elif value=="" and name in self.actives_set:
+            self.actives_set.remove(name)
+        print("updated", self.actives_set)
+
+    def user_defined(self):
+        self.name = self.sender().objectName()
+        self.value = self.sender().text()
+        self.update_status(self.name, self.value)
+
+    def diameter_slot(self):
+        self.user_defined()
+        self.diameter = self.ui_dia.text()
+        self.rev_expr()
+    
+    def speed_slot(self):
+        self.user_defined()
+        self.speed = self.ui_speed.text()
+        self.rev_expr()
+
+    def rev_slot(self):
+        self.user_defined()
+        self.rev = self.ui_rev.text()
+        self.speed_expr()
+
+    def z_slot(self):
+        self.user_defined()
+        self.znum = self.ui_z.text()
+        self.feedrate_expr()
+
+    def F_slot(self):
+        self.user_defined()
+        self.feedrate = self.ui_feedrate.text()
+        self.feedpertooth_expr()
+
+    def f_slot(self):
+        self.user_defined()
+        self.feedpertooth = self.ui_feedpertooth.text()
+        self.feedrate_expr()
+
+    def speed_expr(self):
+        if {"ui_dia", "ui_rev"}.issubset(self.actives_set):
+            speed_result = self.model.speed_formel(self.rev, self.diameter)
+            self.ui_speed.setText(str(speed_result))
+            self.update_status('ui_speed', speed_result)
+            print("Calc Speed", self.actives_set)
+        else:
+            return
 
     def rev_expr(self):
-        self.diameter = self.ui_dia.text()
-        self.speed = self.ui_speed.text()
-        rev_result = self.model.revolutions_formel(float(self.speed), float(self.diameter))
-        self.ui_rev.setText(str(rev_result))
+        if {"ui_dia", "ui_speed"}.issubset(self.actives_set): 
+            rev_result = self.model.revolutions_formel(self.speed, self.diameter)
+            self.ui_rev.setText(str(rev_result))
+            self.update_status('ui_rev', rev_result)
+            print("Calc Rev", self.actives_set)
+        else:
+            return
 
-    def cuttingdata_calc(self):
-        if self.ui_dia.text()!="":
-            if self.ui_speed.text()!="" or self.ui_rev.text()!="":
-                if self.sender() == self.ui_speed:
-                    self.rev_expr()
-                if self.sender() == self.ui_rev:
-                    self.speed_expr()
-                self.rev_expr()
+    def feedrate_expr(self):
+        if {"ui_rev", "ui_z", "ui_feedpertooth"}.issubset(self.actives_set): 
+            feedrate_result = self.model.feedrate_formel(self.rev, 
+                                                         self.feedpertooth, 
+                                                         self.znum)
+            self.ui_feedrate.setText(str(feedrate_result))
+            self.update_status('ui_feedrate', feedrate_result)
+            print("Calc F", self.actives_set)
+        else:
+            return
+
+    def feedpertooth_expr(self):
+        if {"ui_rev", "ui_z", "ui_feedrate"}.issubset(self.actives_set): 
+            feedpertooth_result = self.model.feedpertooth_formel(self.rev, 
+                                                                 self.feedrate, 
+                                                                 self.znum)
+            self.ui_feedpertooth.setText(str(feedpertooth_result))
+            self.update_status('ui_feedpertooth', feedpertooth_result)
+            print("Calc f", self.actives_set)
         else:
             return
 
--- model.py
+++ model.py
@@ -7,17 +7,19 @@ class Model():
 
 
 
-    def speed_formel(self, revolutions: float, diameter: float):
-        speed = (3.14*diameter*revolutions)/1000
+    def speed_formel(self, revolutions, diameter):
+        speed = (3.14*float(diameter)*float(revolutions))/1000
         return ("{:.0f}".format(round(float(speed), 0)))
 
-    def revolutions_formel(self, cutting_speed: float, diameter: float):
-        revolutions = (cutting_speed*1000)/(3.14*diameter)
+    def revolutions_formel(self, cutting_speed, diameter):
+        revolutions = (float(cutting_speed)*1000)/(3.14*float(diameter))
         return ("{:.0f}".format(round(float(revolutions), 0)))
 
-    def feedrate_formel(self, feedpertooth):
-        pass
+    def feedrate_formel(self, speed, feedpertooth, znum):
+        print("speed f z", speed, feedpertooth, znum)
+        feedrate = float(speed)*float(feedpertooth)*float(znum)
+        return ("{:.0f}".format(round(float(feedrate), 0)))
 
-    def feedpertooth_formel(self, feedrate):
-        pass
-    
+    def feedpertooth_formel(self, speed, feedrate, znum):
+        feedpertooth = float(feedrate)/(float(speed)*float(znum))
+        return ("{:.3f}".format(round(float(feedpertooth), 3)))
