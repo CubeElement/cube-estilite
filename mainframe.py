9397cb0ddcec05aa17cdbd9e1c63aec74d173394
CubeElement <cubeelement@protonmail.com>
2021-04-24 10:36:34 +0200

    Adds functioning feedrate and speed parameters, materials data

--- mainframe.py
+++ mainframe.py
@@ -5,6 +5,9 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from mainframe_gui import Ui_Form
 from model import Model
 
+def display_start_fix():
+    os.environ['DISPLAY'] = ':0' 
+
 class MainWindow(QMainWindow, Ui_Form):
     def __init__(self, model=None):
         self.model = model
@@ -12,15 +15,16 @@ class MainWindow(QMainWindow, Ui_Form):
         self.setupUi(self)        
         self.set_current_values()
         self.actives_set = set()
+        self.material_data = self.model.create_cuttingdata()
         self.connectSignals()
 
     def set_current_values(self):
-        self.diameter = self.ui_dia.text()
-        self.rev = self.ui_rev.text()
-        self.speed = self.ui_speed.text()
-        self.feedrate = self.ui_feedrate.text()
-        self.feedpertooth = self.ui_feedpertooth.text()
-        self.znum = self.ui_z.text()
+        self.dia = self.ui_dia.text() # Tool diameter, mm
+        self.rpm = self.ui_rev.text() # Revolutions per minute, 1/min
+        self.speed = self.ui_speed.text() # Cutting speed, m/min
+        self.feedrate = self.ui_feedrate.text() # Feedrate, mm/min
+        self.feedpertooth = self.ui_feedpertooth.text()# Feed per Tooth, mm/tooth
+        self.znum = self.ui_z.text() # Number of Teeth
 
     def connectSignals(self):
         self.ui_dia.editingFinished.connect(self.diameter_slot)        
@@ -29,88 +33,105 @@ class MainWindow(QMainWindow, Ui_Form):
         self.ui_z.editingFinished.connect(self.z_slot)
         self.ui_feedrate.editingFinished.connect(self.F_slot)
         self.ui_feedpertooth.editingFinished.connect(self.f_slot)
+
+        self.ui_mat_p.clicked.connect(lambda x: self.materials_slot("P"))
+        self.ui_mat_m.clicked.connect(lambda x: self.materials_slot("M"))
+        self.ui_mat_k.clicked.connect(lambda x: self.materials_slot("K"))
+        self.ui_mat_n.clicked.connect(lambda x: self.materials_slot("N"))
+        self.ui_mat_s.clicked.connect(lambda x: self.materials_slot("S"))
+        self.ui_mat_h.clicked.connect(lambda x: self.materials_slot("H"))
     
     def update_status(self, name=None, value=None):
         if value!="" and name not in self.actives_set:
             self.actives_set.add(str(name))
         elif value=="" and name in self.actives_set:
             self.actives_set.remove(name)
-        print("updated", self.actives_set)
 
-    def user_defined(self):
+    def store_userinput(self):
         self.name = self.sender().objectName()
         self.value = self.sender().text()
         self.update_status(self.name, self.value)
+        print(self.dia, self.rpm, self.speed, self.feedrate, self.feedpertooth, self.znum)
 
     def diameter_slot(self):
-        self.user_defined()
-        self.diameter = self.ui_dia.text()
+        self.store_userinput()
+        self.dia = self.ui_dia.text()
         self.rev_expr()
     
     def speed_slot(self):
-        self.user_defined()
+        self.store_userinput()
         self.speed = self.ui_speed.text()
         self.rev_expr()
 
     def rev_slot(self):
-        self.user_defined()
-        self.rev = self.ui_rev.text()
+        self.store_userinput()
+        self.rpm = self.ui_rev.text()
         self.speed_expr()
 
     def z_slot(self):
-        self.user_defined()
+        self.store_userinput()
         self.znum = self.ui_z.text()
         self.feedrate_expr()
 
     def F_slot(self):
-        self.user_defined()
+        self.store_userinput()
         self.feedrate = self.ui_feedrate.text()
         self.feedpertooth_expr()
 
     def f_slot(self):
-        self.user_defined()
+        self.store_userinput()
         self.feedpertooth = self.ui_feedpertooth.text()
         self.feedrate_expr()
 
     def speed_expr(self):
         if {"ui_dia", "ui_rev"}.issubset(self.actives_set):
-            speed_result = self.model.speed_formel(self.rev, self.diameter)
+            speed_result = self.model.speed_formel(self.rpm, self.dia)
             self.ui_speed.setText(str(speed_result))
+            self.speed = self.ui_speed.text()
             self.update_status('ui_speed', speed_result)
-            print("Calc Speed", self.actives_set)
-        else:
-            return
 
     def rev_expr(self):
-        if {"ui_dia", "ui_speed"}.issubset(self.actives_set): 
-            rev_result = self.model.revolutions_formel(self.speed, self.diameter)
+        if {"ui_dia", "ui_speed"}.issubset(self.actives_set):
+            self.set_current_values()
+            rev_result = self.model.revolutions_formel(self.speed, self.dia)
             self.ui_rev.setText(str(rev_result))
+            self.rpm = self.ui_rev.text()
             self.update_status('ui_rev', rev_result)
-            print("Calc Rev", self.actives_set)
         else:
             return
 
     def feedrate_expr(self):
-        if {"ui_rev", "ui_z", "ui_feedpertooth"}.issubset(self.actives_set): 
-            feedrate_result = self.model.feedrate_formel(self.rev, 
+        if {"ui_rev", "ui_z", "ui_feedpertooth"}.issubset(self.actives_set):
+            feedrate_result = self.model.feedrate_formel(self.rpm, 
                                                          self.feedpertooth, 
                                                          self.znum)
             self.ui_feedrate.setText(str(feedrate_result))
+            self.feedrate = self.ui_feedrate.text()
             self.update_status('ui_feedrate', feedrate_result)
-            print("Calc F", self.actives_set)
         else:
             return
 
     def feedpertooth_expr(self):
         if {"ui_rev", "ui_z", "ui_feedrate"}.issubset(self.actives_set): 
-            feedpertooth_result = self.model.feedpertooth_formel(self.rev, 
+            feedpertooth_result = self.model.feedpertooth_formel(self.rpm, 
                                                                  self.feedrate, 
                                                                  self.znum)
             self.ui_feedpertooth.setText(str(feedpertooth_result))
+            self.feedpertooth = self.ui_feedpertooth.text()
             self.update_status('ui_feedpertooth', feedpertooth_result)
-            print("Calc f", self.actives_set)
         else:
             return
+
+    def materials_slot(self, material):
+        print(material)
+        self.speed = self.material_data[material][0]
+        self.update_status('ui_speed', self.speed)
+        self.ui_speed.setText(str(self.speed))
+        self.feedpertooth = self.material_data[material][1]
+        self.update_status('ui_feedpertooth', self.feedpertooth)
+        self.ui_feedpertooth.setText(str(self.feedpertooth))
+        self.rev_expr()
+        self.feedrate_expr()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from PyQt5 import QtWidgets
 
 class Model():
     def __init__(self):
-        pass
+        self.datatable = self.create_cuttingdata()
 
 
 
@@ -15,11 +15,24 @@ class Model():
         revolutions = (float(cutting_speed)*1000)/(3.14*float(diameter))
         return ("{:.0f}".format(round(float(revolutions), 0)))
 
+    def revolutions_feed_formel(self, feedrate, znum, feedpertooth):
+        revolutions = (float(feedrate))/(float(feedpertooth)*float(znum))
+        return ("{:.0f}".format(round(float(revolutions), 0)))
+
     def feedrate_formel(self, speed, feedpertooth, znum):
-        print("speed f z", speed, feedpertooth, znum)
         feedrate = float(speed)*float(feedpertooth)*float(znum)
         return ("{:.0f}".format(round(float(feedrate), 0)))
 
     def feedpertooth_formel(self, speed, feedrate, znum):
         feedpertooth = float(feedrate)/(float(speed)*float(znum))
         return ("{:.3f}".format(round(float(feedpertooth), 3)))
+
+    def create_cuttingdata(self):
+        cutting_parameters = {"P":[80, 0.03],
+                              "M":[50, 0.025],
+                              "K":[100, 0.05],
+                              "N":[250, 0.07],
+                              "S":[70, 0.04],
+                              "H":[35, 0.02]
+        }
+        return cutting_parameters
